57e567d352344a96caee42ea22b3f7a5e2d6db2c
Christopher Armenio <christopher.armenio@breakwall.io>
2021-06-29 13:07:42 -0400

    MCVE for issue #12082

--- src/test/java/org/jooq/mcve/test/java/JavaTest.java
+++ src/test/java/org/jooq/mcve/test/java/JavaTest.java
@@ -2,22 +2,35 @@ package org.jooq.mcve.test.java;
 
 import org.jooq.mcve.java.tables.records.TestRecord;
 import org.junit.Test;
+import org.jooq.Field;
+import org.jooq.Record;
+import org.jooq.Result;
+import org.jooq.conf.ParamType;
+import org.jooq.impl.DSL;
 
 import static org.jooq.mcve.java.Tables.TEST;
 import static org.junit.Assert.assertEquals;
 
 public class JavaTest extends AbstractTest {
 
-    @Test
-    public void mcveTest() {
-        TestRecord result =
-        ctx.insertInto(TEST)
-           .columns(TEST.VALUE)
-           .values(42)
-           .returning(TEST.ID)
-           .fetchOne();
+   @Test
+   public void mcveTest() {
+      Field<Integer> field_test = DSL.greatest(
+         TEST.ID,
+         TEST.VALUE
+      );
+      String sql = ctx.select(field_test)
+                        .from(TEST)
+                        .getSQL(ParamType.NAMED_OR_INLINED);
+      System.out.println(sql);
+      Result<Record> res = ctx.fetch(sql);
 
-        result.refresh();
-        assertEquals(42, (int) result.getValue());
-    }
+      System.out.println(res);
+
+      for( Record currRec : res )
+      {
+         // this does not work
+         System.out.println(currRec.get(field_test));
+      }
+   }
 }
